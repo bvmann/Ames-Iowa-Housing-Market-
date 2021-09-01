56c3a950eccf6a05678d99724db5db8c7b815dde
britt mann <brittvmann@gmail.com git config --global user.name bvmann>
2021-08-31 19:00:18 -0700

    testing with flask

---
+++ model_working/app.py
@@ -0,0 +1,71 @@
+from flask import Flask, jsonify, render_template, request, redirect, url_for 
+import joblib
+import pandas as pd 
+import numpy as np 
+
+
+app = Flask(__name__)
+
+model = joblib.load("random_forest.joblib")
+
+pre_cols = ['MSSubClass',
+ 'LotFrontage',
+ 'HouseStyle',
+ 'OverallCond',
+ 'Exterior1st',
+ 'Exterior2nd',
+ 'MasVnrType',
+ 'MasVnrArea',
+ 'Foundation',
+ 'BsmtQual',
+ 'BsmtFinType1',
+ 'HeatingQC',
+ 'BsmtFullBath',
+ 'BedroomAbvGr',
+ 'KitchenQual',
+ 'Fireplaces',
+ 'FireplaceQu',
+ 'GarageType',
+ 'GarageFinish',
+ 'outdoorAreaSF']
+input_cols = ['YearBuilt','FullBath','TotRmsAbvGrd','GrLivArea','TotalBsmtSF','GarageCars','GarageArea','LotArea','Neighborhood','OverallQual']
+pre_values = [20.0,
+ 90.4186046511628,
+ 2.0,
+ 5.0,
+ 11.0,
+ 13.0,
+ 2.0,
+ 0.0,
+ 2.0,
+ 3.0,
+ 5.0,
+ 0.0,
+ 0.0,
+ 3.0,
+ 3.0,
+ 0.0,
+ 5.0,
+ 1.0,
+ 2.0,
+ 0.0]
+@app.route('/')
+def home(): 
+    return render_template("predict_.html")
+
+@app.route('/predict', methods = ['POST'])
+def predict(): 
+    input = [x for x in request.form.values()]
+    features = pre_values + input
+    final = np.array(features)
+    cols = pre_cols + input_cols
+    data = pd.DataFrame([final], columns = cols)
+
+    prediction = model.predict(data).round() 
+   
+
+    return render_template('predict_.html', pred = "Expected home value {}".format(prediction))
+   
+
+if __name__ == '__main__':
+    app.run(debug=True)
