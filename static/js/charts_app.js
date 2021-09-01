49b5d9e1228ad96654fdbef719dc01999a61e5aa
Galen <mgfogerson@gmail.com>
2021-09-01 12:37:13 -0700

    commit

--- static/js/charts_app.js
+++ static/js/charts_app.js
@@ -21,7 +21,7 @@ mediansobject = {
 	MSZoning: []
 }
 //read csv
-d3.csv("../../Resources/train_allvalues.csv", function(data){
+d3.csv("../../Resources/samples.csv", function(data){
 	console.log(data)
 	for (var x in data){
 		mediansobject.Neighborhood.push(data[x].Neighborhood)
