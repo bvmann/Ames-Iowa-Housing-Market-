7efd8bfd2e4960f6c7a06059724cd3b6c39b6189
Britt Mann <68206465+bvmann@users.noreply.github.com>
2021-08-31 19:23:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,11 @@
 # Data Analytics Bootcamp, spring 2021
 
 # let's make an API with Flask
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request, redirect, url_for 
 import numpy as np
 import pandas as pd
 import datetime as dt
+import joblib
 
 # Python SQL toolkit and Object Relational Mapper
 import sqlalchemy
@@ -40,13 +41,76 @@ lngs = pd.read_sql_query("SELECT Longitude from gun_violence_data", engine)
 lats_lngs_df = pd.merge(lats, lngs, left_index=True, right_index=True)
 lats_lngs_list = lats_lngs_df.values.tolist()
 
+model = joblib.load("random_forest.joblib")
+
+pre_cols = ['MSSubClass',
+ 'LotFrontage',
+ 'HouseStyle',
+ 'OverallCond',
+ 'Exterior1st',
+ 'Exterior2nd',
+ 'MasVnrType',
+ 'MasVnrArea',
+ 'Foundation',
+ 'BsmtQual',
+ 'BsmtFinType1',
+ 'HeatingQC',
+ 'BsmtFullBath',
+ 'BedroomAbvGr',
+ 'KitchenQual',
+ 'Fireplaces',
+ 'FireplaceQu',
+ 'GarageType',
+ 'GarageFinish',
+ 'outdoorAreaSF']
+input_cols = ['YearBuilt','FullBath','TotRmsAbvGrd','GrLivArea','TotalBsmtSF','GarageCars','GarageArea','LotArea','Neighborhood','OverallQual']
+pre_values = [20.0,
+ 90.4186046511628,
+ 2.0,
+ 5.0,
+ 11.0,
+ 13.0,
+ 2.0,
+ 0.0,
+ 2.0,
+ 3.0,
+ 5.0,
+ 0.0,
+ 0.0,
+ 3.0,
+ 3.0,
+ 0.0,
+ 5.0,
+ 1.0,
+ 2.0,
+ 0.0]
+
 # setup our Flask app
 app = Flask(__name__)
+
+
 
 # create index route
 @app.route("/")
 def home():
   return render_template("index.html")
+
+@app.route('/start')
+def home(): 
+    return render_template("predict_.html")
+
+@app.route('/predict', methods = ['POST'])
+def predict(): 
+    input = [x for x in request.form.values()]
+    features = pre_values + input
+    final = np.array(features)
+    cols = pre_cols + input_cols
+    data = pd.DataFrame([final], columns = cols)
+
+    prediction = model.predict(data).round() 
+   
+
+    return render_template('predict_.html', pred = "Expected home value {}".format(prediction))
 
 @app.route("/dashboard")
 def dashboard():
